bd03893e88ab060f748f7db36eb8898befa7b42e
Sergio Glorias <sergioglorias@gmail.com>
2020-11-04 14:54:01 +0000

    agora o codigo gera 1 link

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,6 @@
 module.exports = async (client) => {
     console.log(`${client.user.tag} fui iniciado com sucesso!!`)
+    console.log(await client.generateInvite({
+        permissions: 8
+    }))
 }
